3a02a60be6734e1fa71f1dd1a302659b3b99e5a6
camilazombia <camilagomez2307@gmail.com>
2021-07-31 14:52:40 -0500

    update no-java

--- js/main.js
+++ js/main.js
@@ -1,76 +1,80 @@
-//$('#myModal').modal({backdrop: 'static', keyboard: false})
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+
+
 var productos=[
     {
         'id' : 1,
         'nombre' : 'Bogotá',
-        'tipo' : 'lager',
-        'grados' : 4.2,
+        'tipo' : 'Ale',
+        'grados' : 3.5,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/bogota.png",
     },
     {
         'id' : 2,
         'nombre' : 'Medellín',
-        'tipo' : 'lager',
-        'grados' : 4.2,
+        'tipo' : 'Pilsner',
+        'grados' : 4.5,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/medellin.png",
     },
     {
         'id' : 3,
         'nombre' : 'Cali',
-        'tipo' : 'lager',
-        'grados' : 4.2,
+        'tipo' : 'Ipa',
+        'grados' : 6.8,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/cali.png",
     },
     {
         'id' : 4,
         'nombre' : 'Cartagena',
-        'tipo' : 'lager',
+        'tipo' : 'Lager',
         'grados' : 4.2,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
-    },
-    {
-        'id' : 1,
-        'nombre' : 'Caldas',
-        'tipo' : 'lager',
-        'grados' : 4.2,
-        'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/cartagena.png",
     },
     {
         'id' : 5,
-        'nombre' : 'Manizales',
-        'tipo' : 'lager',
-        'grados' : 4.2,
+        'nombre' : 'Caldas',
+        'tipo' : 'Lager',
+        'grados' : 4.0,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/caldas.png",
     },
     {
         'id' : 6,
-        'nombre' : 'Cúcuta',
-        'tipo' : 'lager',
-        'grados' : 4.2,
+        'nombre' : 'Manizales',
+        'tipo' : 'Lager',
+        'grados' : 5.0,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/manizales.png",
     },
     {
         'id' : 7,
-        'nombre' : 'Pereira',
-        'tipo' : 'lager',
-        'grados' : 4.2,
+        'nombre' : 'Cúcuta',
+        'tipo' : 'Lager',
+        'grados' : 4.6,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/cucuta.png",
     },
     {
         'id' : 8,
-        'nombre' : 'Ibagué',
-        'tipo' : 'lager',
+        'nombre' : 'Pereira',
+        'tipo' : 'Pilsener',
         'grados' : 4.2,
         'precio' : 12900,
-        'image' : "https://via.placeholder.com/150",
+        'image' : "./content/pereira.png",
+    },
+    {
+        'id' : 9,
+        'nombre' : 'Ibagué',
+        'tipo' : 'Lager',
+        'grados' : 4.0,
+        'precio' : 12900,
+        'image' : "./content/ibagué.png",
     },
   ]
   
@@ -79,13 +83,20 @@ var productos=[
         <div class="cards_item">
         <img class="cards_img"src="${producto.image}">
         <h3 class="cards_title"> ${producto.nombre}</h3>
-         <p class="cards_type">  Tipo: ${producto.tipo}</p>
-         <p class="cards_gre">  Grados: ${producto.grados}</p>
-         <p class="cards_price"> $ ${producto.precio}</p>
+        <p class="cards_price"> $ ${producto.precio}</p>
+        <div class="cards_desc">
+         <p class="cards_desc--type"> ${producto.tipo} , ${producto.grados} grados </p>
+        </div>
           <div class="cards_btn">
-            <button type="button" class="card-btn${producto.id} card-btn">Comprar</button>
+            <button type="button" class="card-btn${producto.id} cards_btn--item">Comprar</button>
           </div>
          </div>
         `);
-  
+
   }
+
+
+
+
+
+
